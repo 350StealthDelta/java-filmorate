e6eb139837da2ab9eea94c43d8db0b945b08b1e1
Stealth-PC <anubis.delta@yandex.ru>
2022-06-25 12:17:53 +0300

    feat: Добавить jakarta в maven и аннотации валидации в User и Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,9 +10,21 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class Film {
+    
+    @Positive(message = "id должен быть больше нуля")
     private int id;
+    
+    @NotBlank(message = "Необходимо указать имя")
+    @NotNull
     private String name;
+    
+    @Size(max = 200, message = "Размер описания не должен быть больше 200 символов")
     private String description;
+    
+    @NotNull
     private LocalDateTime releaseDate;
+    
+    @Positive(message = "Длительность должна быть больше нуля")
+    @NotNull
     private Duration duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,9 +9,23 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 public class User {
+    
+    @Positive(message = "id должно быть больше нуля")
     private int id;
+    
+    @Email(message = "Email должен быть корректным адресом электронной почты")
+    @NotNull
     private String email;
+    
+    @NotBlank(message = "Логин не может быть пустым")
+    @NotEmpty(message = "Логин не может состоять из пробелов")
+    @NotNull
     private String login;
+    
+    @NotNull
     private String name;
+    
+    @NotNull
+    @Past(message = "Дата рожденья не может быть в будущем или в настоящем")
     private LocalDateTime birthday;
 }
