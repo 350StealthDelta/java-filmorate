9c9f6e3c431466c6e98d916221a950f870e035aa
Stealth-PC <anubis.delta@yandex.ru>
2022-06-25 13:35:23 +0300

    feat: Добавил логирование в контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -10,9 +11,12 @@ import java.time.Month;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
     
+    //    Logger log = LoggerFactory.getLogger(FilmController.class);
     private Map<Integer, Film> films = new HashMap<>();
     
     @PostMapping("/film")
@@ -20,6 +24,7 @@ public class FilmController {
         validationCheck(film);
         
         films.put(film.getId(), film);
+        log.info(String.format("%s добавлен в коллекцию фильмов с id=%s.", film, film.getId()));
         return film;
     }
     
@@ -27,31 +32,38 @@ public class FilmController {
     public Film updateFilm(@RequestBody @Valid Film film) {
         validationCheck(film);
         
+        log.info(String.format("Фильм с id=%s обновлен на %s.", film.getId(), film));
         films.replace(film.getId(), film);
         return film;
     }
     
     @GetMapping("")
     public Map<Integer, Film> getFilms() {
+        log.info("Возвращен список фильмов.");
         return films;
     }
     
     private void validationCheck(Film film) {
         if (film == null) {
+            log.warn("Полученный объект film является null.");
             throw new ValidationException();
         }
         if (film.getReleaseDate() == null || film.getReleaseDate()
             .isBefore(LocalDateTime.of(1895, Month.DECEMBER, 28, 00, 00))) {
             
+            log.warn("Ошибка валидации поля releaseDate объекта film.");
             throw new ValidationException();
         }
         if (film.getName() == null || film.getName().isEmpty()) {
+            log.warn("Ошибка валидации поля name объекта film.");
             throw new ValidationException();
         }
         if (film.getDescription() == null || film.getDescription().length() > 200) {
+            log.warn("Ошибка валидации размера поля description объекта film.");
             throw new ValidationException();
         }
         if (film.getDuration() == null || film.getDuration().isNegative()) {
+            log.warn("Ошибка валидации поля duration объекта film.");
             throw new ValidationException();
         }
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -8,21 +9,25 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     
     private Map<Integer, User> users = new HashMap<>();
     
     @GetMapping("")
     public Map<Integer, User> getUsers(@RequestBody User user) {
+        log.info("Возвращен список пользователей");
         return users;
     }
     
     @PostMapping("/user")
     public User createUser(@RequestBody User user) {
         validateUser(user);
-    
+        
         users.put(user.getId(), user);
+        log.info(String.format("Добавлен пользователь %s с id=%s.", user, user.getId()));
         return user;
     }
     
@@ -31,28 +36,34 @@ public class UserController {
         validateUser(user);
         
         users.replace(user.getId(), user);
+        log.info(String.format("Данные пользователя с id=%s обновлены."));
         return user;
     }
     
     private void validateUser(User user) {
         if (user == null) {
+            log.warn("Полученный объект user является null.");
             throw new ValidationException();
         }
         if (user.getId() <= 0) {
+            log.warn("Поле id объекта user должно быть больше нуля.");
             throw new ValidationException();
         }
         if (user.getEmail() == null || user.getEmail().isEmpty()
             || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
-            
+            log.warn("Ошибка валидации поля email объекта user.");
             throw new ValidationException();
         }
         if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().isBlank()) {
+            log.warn("Ошибка валидации поля login объекта user.");
             throw new ValidationException();
         }
         if (user.getName() == null || user.getName().isEmpty() || user.getName().isBlank()) {
             user.setName(user.getLogin());
+            log.info("Поле name объекта user заменено на login.");
         }
         if (user.getBirthday() == null || user.getBirthday().isAfter(LocalDateTime.now())) {
+            log.warn("Ошибка валидации поля birthday объекта user.");
             throw new ValidationException();
         }
     }
