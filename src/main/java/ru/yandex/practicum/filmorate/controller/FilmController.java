4bef0b0855cf99f0feb63e571ad76a6f9f7b4805
Stealth-PC <anubis.delta@yandex.ru>
2022-06-27 23:31:34 +0300

    refact: Починил контроллер и бин фильмов на проверку через @Valid.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,18 +15,13 @@ import java.util.Map;
 @Slf4j
 public class FilmController {
     
-    //    Logger log = LoggerFactory.getLogger(FilmController.class);
-    private Map<Integer, Film> films = new HashMap<>();
+    private final Map<Integer, Film> films = new HashMap<>();
     private int idCounter = 1;
     
     @PostMapping("")
     public Film addNewFilm(@RequestBody @Valid Film film) {
-        if (film != null) {
-            film.setId(idCounter++);
-        } else {
-            throw new ValidationException();
-        }
-        validationCheck(film);
+        nullValidationCheck(film);
+        film.setId(idCounter++);
         
         films.put(film.getId(), film);
         log.info(String.format("%s добавлен в коллекцию фильмов с id=%s.", film, film.getId()));
@@ -37,19 +30,11 @@ public class FilmController {
     
     @PutMapping("")
     public Film updateFilm(@RequestBody @Valid Film film) {
-        validationCheck(film);
-        
+        idValidationCheck(film);
         
         log.info(String.format("Фильм с id=%s обновлен на %s.", film.getId(), film));
         films.replace(film.getId(), film);
         return film;
-    }
-    
-    @DeleteMapping("/clear")
-    public ResponseEntity<Void> clearFilmsMap() {
-        log.info("Список фильмов очищен.");
-        films.clear();
-        return ResponseEntity.ok().build();
     }
     
     @GetMapping("")
@@ -58,32 +43,26 @@ public class FilmController {
         return films.values().toArray(new Film[0]);
     }
     
-    private void validationCheck(Film film) {
+    //+++++++++++++
+    @DeleteMapping("/clear")
+    public ResponseEntity<Void> clearFilmsMap() {
+        log.info("Список фильмов очищен.");
+        films.clear();
+        idCounter = 1;
+        return ResponseEntity.ok().build();
+    }
+    
+    private void nullValidationCheck(Film film) {
         if (film == null) {
             log.warn("Полученный объект film является null.");
-            throw new ValidationException();
+            throw new ValidationException("Переданный объект film равен null");
         }
+    }
+    
+    private void idValidationCheck(Film film) {
+        nullValidationCheck(film);
         if (film.getId() <= 0) {
-            log.warn("Поле id объекта user должно быть больше нуля.");
-            throw new ValidationException();
-        }
-        if (film.getReleaseDate() == null || film.getReleaseDate()
-            .isBefore(LocalDate.of(1895, Month.DECEMBER, 28))) {
-            
-            log.warn("Ошибка валидации поля releaseDate объекта film.");
-            throw new ValidationException();
-        }
-        if (film.getName() == null || film.getName().isEmpty()) {
-            log.warn("Ошибка валидации поля name объекта film.");
-            throw new ValidationException();
-        }
-        if (film.getDescription() == null || film.getDescription().length() > 200) {
-            log.warn("Ошибка валидации размера поля description объекта film.");
-            throw new ValidationException();
-        }
-        if (film.getDuration() < 0) {
-            log.warn("Ошибка валидации поля duration объекта film.");
-            throw new ValidationException();
+            throw new ValidationException("id объекта должен быть больше 0");
         }
     }
     
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.annotation.AfterFirstFilm;
 
 import java.time.LocalDate;
 
@@ -13,18 +14,15 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Film {
     
-    //    @Positive(message = "id должен быть больше нуля")
     private int id;
     
     @NotBlank(message = "Необходимо указать имя")
-    @NotNull
     private String name;
     
     @Size(max = 200, message = "Размер описания не должен быть больше 200 символов")
     private String description;
     
-    @NotNull
-//    @FutureOrPresent()
+    @AfterFirstFilm(message = "Дата релиза не может быть раньше 28 декабря 1895 года")
     private LocalDate releaseDate;
     
     @Positive(message = "Длительность должна быть больше нуля")
